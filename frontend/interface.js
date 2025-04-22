5cdf3701a37ffcd5081086b876eeb06f94aabc07
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2025-04-22 20:07:00 +0200

    Add files via upload

--- frontend/interface.js
+++ frontend/interface.js
@@ -1,1 +1,5 @@
 //start
+/** Online Market Place System 
+ *  Features: Create a new account so we need a class for accounts 
+ *  Features: Login to your account so we need a login interface for this  
+ */
