6e4a7a271a632a94dfc20241f815220f38d1901e
KarimWalidFawzy <132375356+KarimWalidFawzy@users.noreply.github.com>
2025-04-23 18:14:50 +0200

    Add files via upload

--- frontend/interface.js
+++ frontend/interface.js
@@ -3,3 +3,21 @@
  *  Features: Create a new account so we need a class for accounts 
  *  Features: Login to your account so we need a login interface for this  
  */
+class user {
+
+    constructor(username="",lastname="",password="",email="",phone_number="") {
+        this.username=username;
+        this.password=password;
+        this.email=email;
+        this.lastname=lastname;
+        this.address=address;
+        this.phone_number=phone_number;
+    }
+}
+document.getElementById("loginbutton").onclick=()=>{
+    let lPassword=document.getElementById("password").value; // The l indicates that this is the login password and username
+    let lUsername=document.getElementById("username").value;
+}
+document.getElementById("signupbutton").onclick=()=>{
+
+}
